d92847ad86ccc2865d350bbe7c8c96a68eaeefed
Noah <noahvader@yahoo.com>
2026-05-30 09:12:38 -0400

    Added Play Screen
    Added play screen with scripted play button

---
+++ meow-miners/Scripts/Screens/play_screen.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func play() -> void:
+	pass
+
+
+
+func _on_play_buton_pressed() -> void:
+	play()
