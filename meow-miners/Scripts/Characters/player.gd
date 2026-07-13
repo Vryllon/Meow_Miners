6cce8d06c6c44512a4b1e9a1366308b7a4d36ca5
Noah <noahvader@yahoo.com>
2026-07-13 17:47:08 -0500

    Changed to top down
    Changed player movement to a top down style

--- meow-miners/Scripts/Characters/player.gd
+++ meow-miners/Scripts/Characters/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 var inv = []
 var currency = 0
@@ -35,21 +34,15 @@ func _physics_process(delta: float) -> void:
 
 # Function that handles movement updates during physics_process
 func handle_move(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var directionx := Input.get_axis("move_left", "move_right")
+	var directiony := Input.get_axis("move_up", "move_down")
+	
+	velocity.x = directionx
+	velocity.y = directiony
+	
+	velocity = velocity.normalized() * SPEED
 
 	move_and_slide()
 
