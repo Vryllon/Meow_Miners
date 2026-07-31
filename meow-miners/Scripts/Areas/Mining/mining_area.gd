226bf626ffa21568cf5f5b4f85516cdea975ecd9
Noah <noahvader@yahoo.com>
2026-07-31 18:25:26 -0500

    Made map generation more dynamic
    Updated some outdated functions from to top down style generation as well as making the map generation more dynamic by having adjustable size from in the code

--- meow-miners/Scripts/Areas/Mining/mining_area.gd
+++ meow-miners/Scripts/Areas/Mining/mining_area.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 signal map_generated
 
-var WIDTH: int = 0
-var HEIGHT: int = 0
+var WIDTH: int = 10
+var HEIGHT: int = 10
 
 var mining_pixel_load : PackedScene = load("res://Scenes/Areas/Mining/mining_pixel.tscn")
 var mining_map: Array = []
@@ -14,11 +14,10 @@ func _ready() -> void:
 	call_deferred("initialize_mining_map")
 
 func initialize_mining_map() -> void:
-	WIDTH = $Map.scale.y
-	HEIGHT = $Map.scale.x
-	print_debug(HEIGHT)
-	mining_map = generate_mining_map(1,HEIGHT)
-	generate_pixels(1, HEIGHT)
+	$Map.scale.x = WIDTH
+	$Map.scale.y = HEIGHT
+	mining_map = generate_mining_map()
+	generate_pixels()
 	map_generated.emit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,21 +25,18 @@ func _process(delta: float) -> void:
 	pass
 
 # Generates the data array containing the various materials in the mining area
-func generate_mining_map(start : int, end : int) -> Array:
+func generate_mining_map() -> Array:
 	var map: Array = []
 	
-	for i in (end - start + 1):
-		var level: Array = []
-		
-		# add level data
-		level.append(start + i - 1)
-		
-		# add the data for each mining pixel at this level
+	for i in WIDTH:
+		# generate column
+		var col: Array = []
+		# fill column
 		for j in $Map.scale.y:
-			level.append(rand_mineral(start + i - 1))
+			col.append(rand_mineral(i))
 		
-		# add this level to the map
-		map.append(level)
+		# add this column to the map
+		map.append(col)
 	
 	return map
 
@@ -53,12 +49,11 @@ func rand_mineral(level: int) -> int:
 	return 0
 
 # Generates the mining pixels in the mining area
-func generate_pixels(start: int, end: int) -> void:
-	#print_debug(str($Background.scale.x) + " " + str($Background.scale.y))
+func generate_pixels() -> void:
 	for i in WIDTH:
 		for j in HEIGHT:
 			var mining_pixel = mining_pixel_load.instantiate()
 			add_child(mining_pixel)
 			mining_pixel.name = "mining_pixel_" + str(i) + "_" + str(j)
 			mining_pixel.position = Vector2((i - HEIGHT/2) * 64 + 32, (j - WIDTH/2) * 64 + 32)
-			mining_pixel.set_mineral(mining_map[start+i-1][j+1])
+			mining_pixel.set_mineral(mining_map[i][j])
